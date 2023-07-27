252655b8712da5b4d216825edef5e6ec814ac4f7
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-27 09:45:21 -0300

    Clase 01 - Teórica

--- Algo_1/01_programas_sencillos/Clase_01.py
+++ Algo_1/01_programas_sencillos/Clase_01.py
@@ -8,23 +8,28 @@ def hola_alan():
 def hola_(nombre: str) -> str:
     return f"Hola {nombre}!"
 
-def cuad(numero):
+def cuad(numero: int) -> int:
     """Describe el cuadrado del número dado.
     """
     return numero*numero
 
-def suma_5_cuadrados():
+def suma_5_cuadrados() -> int:
     """Describe la suma de los primeros 5 números naturales, 
     elevados al cuadrado.
     """
     total = 0
+    # total = total + cuad(1)
+    # total = total + cuad(2)
+    # total = total + cuad(3)
+    # total = total + cuad(4)
+    # total = total + cuad(5)
     for i in range(6):
         total += cuad(i)
     return total
 
 # print(suma_5_cuadrados())
 
-def suma_n_cuadrados(n):
+def suma_n_cuadrados(n: int) -> int:
     """Describe la suma de los primeros n números naturales
     elevados al cuadrado.
     """
@@ -32,3 +37,32 @@ def suma_n_cuadrados(n):
     for i in range(n+1):
         total += cuad(i)
     return total
+
+print("Suma:", suma_n_cuadrados(100))
+
+def modulo_de_un_vector(x, y) -> float:
+    """Describe el módulo de un vector 2D.
+       Argumentos:
+            x: <float|int> coordenada de las abscisas.
+            y: <float|int> coordenada de las ordenadas.
+       Devuelve: (float) módulo de un vector.
+    """
+    return pow((pow(x,2) + pow(y,2)), 0.5)
+
+# Algunas cuestiones para pensar... 🙊
+def f():
+    return "Hola" # 
+
+x = f() # x ➡ "Hola"
+
+def g():
+    print("Hola") 
+
+x = g() # x ➡ None
+
+def f():
+    8 
+    abs(-10.432 + 33)
+    "Holis"
+
+f()

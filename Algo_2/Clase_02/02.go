db5629156983c905dec463e08281571fca105744
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-28 19:09:46 -0300

    Clase 02

--- Algo_2/Clase_02/02.go
+++ Algo_2/Clase_02/02.go
@@ -18,8 +18,36 @@ func main() {
 	fmt.Printf("Ahora vale %d\n", dolar)
 
 	fmt.Printf("Ahora vale %d\n", dolar)
-	pasaUnDiaI(&dolar)
+	pasaUnDiaConPuntero(&dolar)
 	fmt.Printf("Ahora vale %d\n", dolar)
+
+	// ----------------------------------------
+	// Arrays 🏋🏼‍♀️
+
+	// Secuencia de elementos.
+	// Se crean con tamaño fijo que no se puede modificar.
+	// Los elementos se encuentran contiguos en memoria.
+	// Las posiciones van de 0 a N-1 (siendo N el tamaño del array).
+
+	var arr []int = []int{1, 2, 3, 4}
+
+	fmt.Printf("%d ", arr[0])
+	fmt.Printf("%d ", arr[1])
+	fmt.Printf("%d ", arr[2])
+	fmt.Printf("%d ", arr[3])
+
+	//var arreglo [4]int = [5]int {1,2,3,4} // Parte del tipo de un array es el tamaño que tiene.
+
+	arreglo := [4]int{1, 2, 3}
+	// Se imprime el valor por defecto en el último lugar.
+	fmt.Println(arreglo)
+
+	modificar(arreglo) // Pasa el array por copia.
+	fmt.Println(arreglo)
+
+	// Ahora si... 🙌🏼
+	modificarConPuntero(&arreglo)
+	fmt.Println(arreglo)
 
 }
 
@@ -27,6 +55,14 @@ func pasaUnDia(valor int) {
 	valor = 2 * valor
 }
 
-func pasaUnDiaI(valor *int) {
+func pasaUnDiaConPuntero(valor *int) {
 	*valor = 2 * *valor
+}
+
+func modificar(arr [4]int) {
+	arr[0] = 0
+}
+
+func modificarConPuntero(arr *[4]int) {
+	arr[0] = 0
 }

46a56c129f8919d91d98ac8f8a0398933c251aa3
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-08-10 23:10:49 -0300

    Se agrega MikiMoko en Decisiones

--- Algo_1/04_decisiones/condicionales.py
+++ Algo_1/04_decisiones/condicionales.py
@@ -11,6 +11,19 @@ def miki_moko1():
         if i % 3 != 0 and i % 5 != 0:
             print(i)
 
+def miki_moko2():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                print("MikiMoko")
+            else:
+                print("Miki")
+        else: # no es divisible por 3 
+            if i % 5 == 0:
+                print("Moko")
+            else: 
+                print(i) 
+          
 def miki_moko(n: int):
     for i in range(1, n+1):
         if not i % 3 and not i % 5: print("MikiMoko")
@@ -18,5 +31,5 @@ def miki_moko(n: int):
         elif not i % 5:             print("Moko")
         else:                       print(i)
 
-# miki_moko1()
+miki_moko2()
 # miki_moko(100)

cfdd03b1a6bbf49494e57aebc4a162cd76d59e6f
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-08-16 13:04:43 -0300

    Se agrega 6.9.5.py

--- Algo_1/06_secuencias/6.9.5.py
+++ Algo_1/06_secuencias/6.9.5.py
@@ -17,5 +17,34 @@ def primera_letra_de_cada_palabra(cadena: str) -> str:
 
 # b) Dicha cadena con la primera letra de cada palabra en mayúsculas. Por ejemplo, si recibe
 # 'república argentina' debe devolver 'República Argentina'.
+
+def primera_letra_en_mayúscula_vers1(cadena: str) -> str:
+    """ 
+    """
+    palabras = cadena.split()
+    cadena_nueva = str()
+    for palabra in palabras:
+        cadena_nueva += f"{palabra.capitalize()} "
+    return cadena_nueva
+
+def primera_letra_en_mayúscula_vers2(cadena: str):
+    palabras = cadena.split()
+    return ' '.join([palabra.capitalize() for palabra in palabras]) 
+
+# print(primera_letra_en_mayúscula_vers1('república argentina'))
+# print(primera_letra_en_mayúscula_vers2('república argentina'))
+
 # c) Las palabras que comiencen con la letra ‘A’. Por ejemplo, si recibe 'Antes de ayer'
-# debe devolver 'Antes ayer'
+# debe devolver 'Antes ayer'.
+
+def palabras_que_comiencen_con__(cadena: str, letra: str) -> str:
+    """
+    """
+    palabras = cadena.split()
+    cadena_nueva = str()
+    for palabra in palabras:
+        if palabra[0] == letra or palabra[0] == letra.upper():
+            cadena_nueva += f"{palabra} "
+    return cadena_nueva
+
+# print(palabras_que_comiencen_con__('Antes de ayer', 'a'))

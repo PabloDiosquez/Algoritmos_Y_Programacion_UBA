c6c6ed997cc8cf1b527465ed6d0778abf54b91f1
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-08-19 19:12:40 -0300

    Se agrega 7.7.9.py

--- Algo_1/07_secuencias_2/7.7.9.py
+++ Algo_1/07_secuencias_2/7.7.9.py
@@ -4,4 +4,21 @@
 # 1), (1, 2), (3, 2)].
 
 def empaquetar(lista: list) -> list[tuple]:
-    pass 
+    """ Describe una lista de tuplas (elemento, cantidad_de_apariciones) para cada elemento de la lista dada.
+        Parámetros:
+            - lista (list[a]): Lista de elementos.
+        Retorna:
+            - lista (list[tuple(a, int)]): Lista de tuplas formadas por cada elemento de la lista dada junto con su cantidad correspondiente de apariciones.
+    """
+    empaquetado = list()
+    apariciones = 1
+    for i in range(len(lista)-1):
+        if lista[i] == lista[i+1]:
+            apariciones += 1
+        else:
+            empaquetado.append((lista[i], apariciones))
+            apariciones = 1
+    empaquetado.append((lista[-1], apariciones))
+    return empaquetado 
+
+# print(empaquetar([1, 1, 1, 3, 5, 1, 1, 3, 3]))

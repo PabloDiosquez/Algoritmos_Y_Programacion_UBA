b3f9a009ad60fa0ac7bc0eebd634c0eb02bd5d31
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-08-22 09:13:29 -0300

    Se agrega 8.4.5.py y documentación en helpers.py

--- Algo_1/08_búsqueda/helpers.py
+++ Algo_1/08_búsqueda/helpers.py
@@ -1,16 +1,28 @@
 def es_vacía(lista: list) -> bool:
     """
+    Indica si la lista dada es vacía.
+    Parámetros:
+        - lista (list): La lista según la cual se describe True o False.
     """
     return len(lista) == 0
 
 def uno_si_cero_sino(condición: bool) -> int:
     """
+    Describe 1 si se cumple la condición dada. En caso contrario, describe 0.
+    Parámetros:
+        - condición (bool): Condición según la cual se describe 1 o 0.
     """
     if condición: return 1
     return 0
 
-def máximo_entre(número1, número2):
+def máximo_entre(elemento1, elemento2):
     """
+    Describe el máximo entre los dos elementos dados.
+    Parámetros:
+        - elemento1: Uno de los elementos a comparar.
+        - elemento2: Uno de los elementos a comparar.
+    Precondiciones:
+        - Los elementos dados deben ser comparables entre sí a través del operador '>'.
     """
-    if número1 > número2: return número1
-    return número2
+    if elemento1 > elemento2: return elemento1
+    return elemento2

1e588039b694049090910a7c38394563e3744ff1
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-08-29 14:26:24 -0300

    Se agrega listas_enlazadas.py

--- Algo_1/15_listas_enlazadas/listas_enlazadas.py
+++ Algo_1/15_listas_enlazadas/listas_enlazadas.py
@@ -1,39 +1,48 @@
 # ¿Cómo implementar una lista?
 
-# ------ Nodo 🙊 ------
+# ------ Lista 🙊 ------
 class ListaEnlazada:
-    # El método __init__ se encarga de inicializar la lista enlazada. El atributo self.prim se utiliza para almacenar el primer nodo de la lista. Al inicio, la lista está vacía, por lo que se establece self.prim como None.
+    # El método __init__ se encarga de inicializar la lista enlazada. El atributo self.prim se utiliza para almacenar el primer _nodo de la lista. Al inicio, la lista está vacía, por lo que se establece self.prim como None.
     def __init__(self):
         self.prim = None
 
     # El método append se utiliza para agregar un nuevo dato al final de la lista
     def append(self, dato):
-        nuevo = Nodo(dato, None)
+        nuevo = _Nodo(dato, None)
 
-        if self.prim is None: self.prim = nuevo # Caso borde (lista vacía)
+        if self.prim is None:
+            self.prim = nuevo # Caso borde (lista vacía)
 
         actual = self.prim 
-        while actual.próx is not None:
-            actual = actual.próx
-         # actual es el último nodo
-        actual.próx = nuevo 
+        while actual.prox is not None:
+            actual = actual.prox
+         # actual es el último _nodo
+        actual.prox = nuevo 
     
     def __str__(self) -> str:
-        s = str()
+        s = "["
         
         actual = self.prim
-        while actual.próx is not None:
-            s += f"{str(actual) }"
-        s += f"{str(actual)}"
-        return s
-
-         
-# ------ Nodo 🙈 ------
-class Nodo:
+        while actual is not None:
+            s += f"{str(actual.dato) }"
+            actual = actual.prox
+        s += "]"
+        return ', '.join(s.split(' '))
     
-    def __init__(self, dato, próx):
+    def __len__(self):
+        cant_nodos = 0
+        actual = self.prim
+        while actual is not None:
+            cant_nodos += 1
+            actual = actual.prox
+        return cant_nodos
+
+# ------ _Nodo 🙈 ------
+class _Nodo:
+    
+    def __init__(self, dato, prox):
         self.dato = dato 
-        self.próx = próx 
+        self.prox = prox 
 
     def __str__(self) -> str:
         return f"Nodo({self.dato})"
@@ -41,11 +50,11 @@ class Nodo:
 
 # ------ Main 🙉 ------
 def main():
-    # n1 = Nodo('a', None)
+    # n1 = _Nodo('a', None)
     # print(n1)
-    # n2 = Nodo('b', None)
-    # n1.próx = n2 
-    # print(n1.próx)
+    # n2 = _Nodo('b', None)
+    # n1.prox = n2 
+    # print(n1.prox)
     # print(n2)
 
     L = ListaEnlazada()

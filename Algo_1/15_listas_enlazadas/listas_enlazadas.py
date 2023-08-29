0045747a5c0c54c414b303f5d81838a9711b89d0
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-08-29 13:25:38 -0300

    Se agrega listas_enlazadas.py

--- Algo_1/15_listas_enlazadas/listas_enlazadas.py
+++ Algo_1/15_listas_enlazadas/listas_enlazadas.py
@@ -10,15 +10,24 @@ class ListaEnlazada:
     def append(self, dato):
         nuevo = Nodo(dato, None)
 
-        if self.prim is None: self.prim = nuevo # Caso borde 
+        if self.prim is None: self.prim = nuevo # Caso borde (lista vacía)
 
         actual = self.prim 
         while actual.próx is not None:
             actual = actual.próx
          # actual es el último nodo
         actual.próx = nuevo 
-         
+    
+    def __str__(self) -> str:
+        s = str()
+        
+        actual = self.prim
+        while actual.próx is not None:
+            s += f"{str(actual) }"
+        s += f"{str(actual)}"
+        return s
 
+         
 # ------ Nodo 🙈 ------
 class Nodo:
     
@@ -32,11 +41,19 @@ class Nodo:
 
 # ------ Main 🙉 ------
 def main():
-    n1 = Nodo('a', None)
-    print(n1)
-    n2 = Nodo('b', None)
-    n1.próx = n2 
-    print(n1.próx)
-    print(n2)
+    # n1 = Nodo('a', None)
+    # print(n1)
+    # n2 = Nodo('b', None)
+    # n1.próx = n2 
+    # print(n1.próx)
+    # print(n2)
+
+    L = ListaEnlazada()
+    L.append('a')
+    L.append('b')
+    L.append('c')
+
+    print(str(L))
+
 
 main()

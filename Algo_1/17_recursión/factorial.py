5210dddc1227fc4c549802fe46731ce7057c510d
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-09-03 15:33:31 -0300

    Se agrega factorial.py

--- Algo_1/17_recursión/factorial.py
+++ Algo_1/17_recursión/factorial.py
@@ -1,8 +1,8 @@
 # La recurusión ocurre cuando algo está definido en términos de sí mismo o su tipo.
 
-def factorial_iterativo(n: int):
+def factorial_iterativo(n: int):      # T(n)      E(n)
     """
-    Precondición: n entero >= 0
+    Precondición: n entero >= 0       # O(n)      O(1)
     Devuelve: n!
     """
     fact = 1
@@ -10,10 +10,10 @@ def factorial_iterativo(n: int):
         fact *= index
     return fact
 
-def factorial_recursivo(n: int):
+def factorial_recursivo(n: int):      # O(n)      O(n)
     """
     Precondición: n entero >= 0
     Devuelve: n!
     """
-    if n == 0: return 1
-    return factorial_recursivo(n-1)*n
+    if n == 0: return 1               # Caso base 
+    return factorial_recursivo(n-1)*n # Caso recursivo

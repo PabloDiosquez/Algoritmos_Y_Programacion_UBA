c65fd6b1eda57bd0aafbfc2d7582b0e88269629f
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-09-11 10:45:07 -0300

    Se modifica listas_enlazadas.py

--- Algo_1/15_listas_enlazadas/listas_enlazadas.py
+++ Algo_1/15_listas_enlazadas/listas_enlazadas.py
@@ -2,9 +2,14 @@
 
 # ------ Lista 🙊 ------
 class ListaEnlazada:
-    # El método __init__ se encarga de inicializar la lista enlazada. El atributo self.prim se utiliza para almacenar el primer _nodo de la lista. Al inicio, la lista está vacía, por lo que se establece self.prim como None.
+    "Modela una lista enlazada"
+
+    # El método __init__ se encarga de inicializar la lista enlazada.
     def __init__(self):
+        # Referencia al primer nodo (None si la lista está vacía)
         self.prim = None
+        # Cantidad de elementos de la lista
+        self.len  = 0
 
     # El método append se utiliza para agregar un nuevo dato al final de la lista
     def append(self, dato):
@@ -20,8 +25,8 @@ class ListaEnlazada:
         actual.prox = nuevo 
     
     def __str__(self) -> str:
+        "Representación en cadena de texto de la lista"
         s = "["
-        
         actual = self.prim
         while actual is not None:
             s += f"{str(actual.dato) }"
@@ -30,23 +35,45 @@ class ListaEnlazada:
         return ', '.join(s.split(' '))
     
     def __len__(self):
-        cant_nodos = 0
+        "Describe la longitud de la lista"
+        # Implementación poco eficiente del método.
+        # cant_nodos = 0
+        # actual = self.prim
+        # while actual is not None:
+        #     cant_nodos += 1
+        #     actual = actual.prox
+        # return cant_nodos
+        return self.len
+    
+    def ver_lista(self):
+        """
+        Recorre todos los nodos de la lista a través de sus enlaces, mostrando sus contenidos. 
+        """
         actual = self.prim
         while actual is not None:
-            cant_nodos += 1
+            print(actual)
             actual = actual.prox
-        return cant_nodos
-
+    
 # ------ _Nodo 🙈 ------
 class _Nodo:
-    
-    def __init__(self, dato, prox):
-        self.dato = dato 
-        self.prox = prox 
+    def __init__(self, dato=None, prox=None):
+        """
+        Crea un nodo que guarda el dato 'dato' y tiene una referencia al nodo 'prox'.
+        :param dato: El dato que se almacenará en el nodo.
+        :type dato: any, opcional
+        :param prox: El nodo siguiente al nodo actual.
+        :type prox: _Nodo, opcional
+        """
+        self.dato = dato
+        self.prox = prox
 
     def __str__(self) -> str:
+        """
+        Describe una representación en cadena de texto de un nodo.
+        :retorna: Representación en cadena de texto del nodo.
+        :rtype: str
+        """
         return f"Nodo({self.dato})"
-
 
 # ------ Main 🙉 ------
 def __init__():
@@ -62,7 +89,6 @@ def __init__():
     L.append('b')
     L.append('c')
 
-    print(str(L))
-
+    L.ver_lista()
 
 __init__()

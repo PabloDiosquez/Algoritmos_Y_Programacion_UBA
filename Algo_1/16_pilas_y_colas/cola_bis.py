726fce3d811d40f9453dec9a8670a055700e551f
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-09-16 10:34:59 -0300

    Se agrega cola_bis.py

---
+++ Algo_1/16_pilas_y_colas/cola_bis.py
@@ -0,0 +1,35 @@
+# Colas implementadas sobre listas ⭐
+
+class Cola:
+    """
+    Representa a una cola, con operaciones de encolar y
+    desencolar. El primero en ser encolado es también el primero
+    en ser desencolado.
+    """
+
+    def __init__(self):
+        """
+        Crea una cola vacía.
+        """
+        self.items = []
+
+    def está_vacía(self):
+        """
+        Indica si la cola está vacía.
+        """ 
+        return not self.items.__len__
+
+    def encolar(self, x: any):
+        """
+        Encola el elemento 'x'
+        """
+        self.items.append(x)
+
+    def desencolar(self):
+        """
+        Desencola el elemento 'x'.
+        Si la cola está vacía lanza ValueError.
+        """
+        if self.está_vacía():
+            raise ValueError("Cola vacía")
+        return self.items.pop(0)

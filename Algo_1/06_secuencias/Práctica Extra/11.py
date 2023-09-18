af1c77d02b5cb7d9180ad039ae13594e19390e48
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-09-18 10:05:16 -0300

    Se agrega 11.py

--- Algo_1/06_secuencias/Práctica Extra/11.py
+++ Algo_1/06_secuencias/Práctica Extra/11.py
@@ -1,4 +1,5 @@
 # 11.
+# a.
 def calcular_total_a_pagar(ticket: list) -> float:
     """
     Describe el total a pagar a partir de una lista de productos en un ticket.
@@ -8,6 +9,29 @@ def calcular_total_a_pagar(ticket: list) -> float:
         float: El total a pagar calculado a partir de los precios de los productos en el ticket.
     """
     total_a_pagar = 0
-    for producto, precio in ticket: # desempaquetado directamente las tuplas (producto, precio)
+    for _, precio in ticket: # desempaquetado directamente las tuplas (producto, precio)
         total_a_pagar += precio 
     return total_a_pagar 
+
+# b.
+def calcular_total_en_lista_de_tickets(tickets):
+    """
+    Calcula el total a pagar a partir de una lista de tickets.
+    Parámetros:
+        - tickets (list[list]): Una lista de tickets, donde cada ticket es una lista de tuplas (producto, precio).
+    Retorna:
+        float: El total a pagar calculado a partir de los precios de los productos en la lista de tickets dada.
+    """
+    total_a_pagar = 0
+    for ticket in tickets:
+        total_a_pagar += calcular_total_a_pagar(ticket)
+    return total_a_pagar
+
+def main():
+    ticket = [
+        ("Detergente", 123),
+        ("Jabón líquido", 456)
+    ]
+    print(f"Total a pagar: ARS ${calcular_total_a_pagar(ticket)}")
+
+main()

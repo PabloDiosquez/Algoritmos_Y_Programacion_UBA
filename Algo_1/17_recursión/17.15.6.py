f885e9a5f30de7b44d865a5b542adcd75eb192a8
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-30 11:22:13 -0300

    Se agrega una modificación de 17.15.6.py

--- Algo_1/17_recursión/17.15.6.py
+++ Algo_1/17_recursión/17.15.6.py
@@ -1,16 +1,25 @@
 # Escribir una función recursiva que calcule recursivamente el n-ésimo número
 # triangular (el número 1 + 2 + 3 + ⋯ + n).
 
-def n_esimo_número_triangular(n: int):
-    """
-    Describe el 'n'-ésimo número triangular.
-    Pre:
-        - El número dado debe ser >= 0.
-    """
-    if n == 1: return 1 
-    return n_esimo_número_triangular(n-1) + n 
+def numero_triangular(n):
+  """
+  Calcula recursivamente el n-ésimo número triangular.
 
-# Ejemplo de uso
-n = 5
-resultado = n_esimo_número_triangular(n)
-print(f"El {n}-ésimo número triangular es {resultado}")
+  Parámetros:
+  - n (int): El índice del número triangular que se calculará.
+
+  Retorna:
+  int: El n-ésimo número triangular.
+  """
+  # Caso base: el número triangular de 0 es 0.
+  if n == 0:
+      return 0
+  # Llama recursivamente a la función para el índice n-1 y suma n.
+  return numero_triangular(n - 1) + n
+
+# Ejemplos de uso con assert para verificar resultados.
+assert numero_triangular(0) == 0
+assert numero_triangular(1) == 1
+assert numero_triangular(2) == 3   # 1 + 2 = 3
+assert numero_triangular(3) == 6   # 1 + 2 + 3 = 6
+assert numero_triangular(4) == 10  # 1 + 2 + 3 + 4 = 10

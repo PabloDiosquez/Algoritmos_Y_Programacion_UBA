ca29daf40d5f1873bc5a1c2184ce6ed906bb5c18
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-30 11:18:17 -0300

    Se agrega una modificación de 17.15.5.py

--- Algo_1/17_recursión/17.15.5.py
+++ Algo_1/17_recursión/17.15.5.py
@@ -4,29 +4,44 @@
 # • 1 es impar.
 # • Si un número es impar, su antecesor es par; y viceversa.
 
-def par(n: int):
+def es_par(n: int) -> bool:
     """
-    Indica si el número dado es par.
-    Pre:
-        - El número dado debe ser >= 0.
-    """
-    if n == 1: return False
-    return impar(n-1)
+    Verifica la paridad de un número natural utilizando recursión.
 
-def impar(n: int):
-    """
-    Indica si el número dado es impar.
-    Pre:
-        - El número dado debe ser >= 0.
-    """
-    if n == 1: return True 
-    return par(n-1)
+    Parámetros:
+    - n (int): El número natural que se verificará si es par.
 
-# Casos de prueba:
-assert     par(2)
-assert     impar(11)
-assert     par(22)
-assert     impar(21)
-assert not impar(2)
-assert not par(15)
-assert not impar(14)
+    Retorna:
+    bool: True si n es par, False si es impar.
+    """
+    # Caso base: 0 es par.
+    if n == 0:
+        return True
+    # Llama a la función es_impar con el antecesor de n.
+    return es_impar(n - 1)
+
+def es_impar(n: int) -> bool:
+    """
+    Verifica la paridad de un número natural utilizando recursión.
+
+    Parámetros:
+    - n (int): El número natural que se verificará si es impar.
+
+    Retorna:
+    bool: True si n es impar, False si es par.
+    """
+    # Caso base: 0 es par, por lo que n es impar.
+    if n == 0:
+        return False
+    # Llama a la función es_par con el antecesor de n.
+    return es_par(n - 1)
+
+# Ejemplos de uso con assert para verificar resultados.
+assert es_par(2)
+assert es_par(10)
+assert es_par(14)
+assert not es_par(15)
+assert es_impar(15)
+assert es_impar(21)
+assert es_impar(75)
+assert not es_impar(14)

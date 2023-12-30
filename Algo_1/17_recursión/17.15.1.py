f12bfb6b3e7a87982240ca2faf198ee89c2576a3
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-12-30 10:56:20 -0300

    Se agrega una modificación de 17.15.1.py

--- Algo_1/17_recursión/17.15.1.py
+++ Algo_1/17_recursión/17.15.1.py
@@ -1,6 +1,7 @@
 # Escribir una función recursiva que reciba un número positivo n y devuelva la
 # cantidad de dígitos que tiene.
 
+# v1
 def cantidad_dígitos(n: int):
     """
     Describe la cantidad de dígitos que tiene el número dado.
@@ -9,6 +10,30 @@ def cantidad_dígitos(n: int):
     """
     if n <= 9: return 1 
     return 1 + cantidad_dígitos(n // 10)
+
+# v2
+def cantidad_digitos(n: int) -> int:
+    """
+    Calcula la cantidad de dígitos en un número entero dado.
+
+    Parámetros:
+    - n (int): El número del cual se calculará la cantidad de dígitos.
+
+    Retorna:
+    int: La cantidad de dígitos en el número dado.
+
+    Ejemplo:
+    >>> cantidad_digitos(123)
+    3
+    """
+    if not n:
+        # Si el número es 0, no tiene dígitos.
+        return 0
+
+    # Si el número no es 0, se llama recursivamente a la función
+    # dividiendo el número por 10 en cada llamada hasta que el número sea 0.
+    # Se suma 1 en cada llamada para contar el dígito actual.
+    return 1 + cantidad_digitos(n // 10)
 
 # Ejemplo de uso:
 numero = 12345
